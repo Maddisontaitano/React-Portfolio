520d0f13144d6559a1826080e817c6cbbf39dc07
Maddison Taitano <maddisontaitano@gmail.com>
2021-03-18 16:22:06 -0400

    adding intro component

---
+++ react-portfolio/src/components/intro/intro.js
@@ -0,0 +1,22 @@
+import React from "react";
+ // import './intro/intro.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h2>About Me!</h2>
+        
+        <p> Hello! My Name is Maddison Taitano and I am 19 years old. 
+            I am looking forward to starting my career in web stack developemnt. <br />
+            I am from up north in Connecticut but I moved to North Carolina about 6 years ago.
+            I took to a writing forum in middle school, which needed coding if you wanted to add any styling or pictures,
+            so that is where I was first introduced. I took a coding class in highschool which
+             peeked my interest, and that started the journey!
+        </p>   
+
+    </div>
+  );
+}
+
+
+export default Header;
